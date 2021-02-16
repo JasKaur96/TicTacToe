c6da10802b59c59d54fc1349eb6d72734cf3d4ad
JasKaur96 <77604638+JasKaur96@users.noreply.github.com>
2021-02-16 16:28:21 +0530

    Refactered: Added comments.

--- TicTacToe/src/com/tictactoepkg/TicTacToeMain.java
+++ TicTacToe/src/com/tictactoepkg/TicTacToeMain.java
@@ -5,30 +5,32 @@ import java.util.Scanner;
 public class TicTacToeMain {
 	static char user = 'x';
 	static char comp = 'o';
-
+	
+	//Initialize board.
 	public static void displayBoard() {
 		char[] board = new char[10];
 		for (int i = 1; i < 10; i++) {
 			board[i] = ' ';
 		}
 	}
-
+	
+	//Take user choice.
 	public static void inputLetter() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("\nEnter X or O: ");
 		char val = sc.next().charAt(0);
+		//To check if user input is capital or small letter and assign it.
 		if (val == 'x' || val == 'X') {
 			user = 'X';
 			comp = 'O';
-			System.out.println("You are " + user + " and Computer is " + comp);
 		} else if (val == 'o' || val == 'O') {
 			user = 'O';
 			comp = 'X';
-			System.out.println("You are " + user + " and Computer is " + comp);
 		} else
 			System.out.println("Invalid Input");
 	}
-
+	
+	//Main to calls the methods to display board and take user choice.
 	public static void main(String[] args) {
 		System.out.println("*****Welcome to the TicTacToe Game.*****");
 		displayBoard();
