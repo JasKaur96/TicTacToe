6558a32bc58621119e8b4fa235ea5439bdc2015f
JasKaur96 <jaspreetflora7@gmail.com>
2021-02-20 07:20:20 +0530

    Refactered: Blocking the winner.

--- TicTacToe/src/com/tictactoepkg/TicTacToeMain.java
+++ TicTacToe/src/com/tictactoepkg/TicTacToeMain.java
@@ -1,17 +1,17 @@
 package com.tictactoepkg;
 
+import java.util.Scanner;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class TicTacToeMain {
 	static char user = 'x';
-	static char comp = 'o';
+	static char comp = 'o',result = ' ';
 	static char[] board = new char[10];
 	static String turn = " ";
-	static char result = ' ';
-	static boolean check;
-	static int board_filled;
-	
+//	static int position;
+
 	// Initialize empty board.
 	private static void display() {
 		for (int index = 1; index < 10; index++) {
@@ -53,7 +53,6 @@ public class TicTacToeMain {
 		if (position > 0 && position < 10) {
 			if (board[position] == ' ') {
 				makeMove(position);
-				board_filled++;
 			} else
 				System.out.println("Position filled.");
 		} else
@@ -62,38 +61,30 @@ public class TicTacToeMain {
 
 	// Computer's turn
 	private static void compTurn() {
+		blockWinner();
 		int compPosition = (int) (Math.floor(Math.random() * 10) );
-//		boolean win = false;
 		for (int index = 1; index < 10; index++) {
-			if (board[compPosition] == ' ') {
+			if (board[compPosition] == ' ')
 				makeMove(compPosition);
-				board_filled++;
-				break;
-			}
-			else
-				continue;
 		}
-	}	
+	}
 
 	// To make a move to the position.
 	private static void makeMove(int position) {
 		for (int index = 1; index < 10; index++) {
 			if (turn == "user") {
 				board[position] = user;
-				turn = "comp";
-				winnerCheck();
+				turn = "comp";		
 			} else {
 				board[position] = comp;
 				turn = "user";
-				winnerCheck();
 			}
-		}
-		displayBoard();
+			winTie();
+		} 
 	}
 
 	// To toss and check who starts.
 	private static void toss() {
-
 		int tossCheck = (int) Math.floor(Math.random() * 10) % 2;
 		if (tossCheck == 0) {
 			turn = "user";
@@ -104,75 +95,80 @@ public class TicTacToeMain {
 			System.out.println("It's computer's turn first!");
 			compTurn();
 		}
-
 	}
 
-	// To check who won the game.
-	private static void winnerCheck() {
-		// Check horizontal
-
-		// top row
-		if((board[1] == board[2]) && (board[2] == board[3]))
-		    result = board[1];
-			System.out.println(board[1]+" wins");
-		// middle row
-		if((board[4] == board[5]) && (board[5] == board[6]))
-			result = board[4];
-			System.out.println(board[4]+" wins");
-		// bottom row
-		if((board[7] == board[8]) && (board[8] == board[9]))
-			result = board[7];
-			System.out.println(board[7]+" wins");
-		// Check verticals
-
-		// left column
-		if((board[1] == board[3]) && (board[3] == board[7]))
-			result = board[1];
-		System.out.println(board[1]+" wins");
-		// middle column
-		if((board[2] == board[5]) && (board[5] == board[8]))
-			result = board[2];
-		System.out.println(board[2]+" wins");
-		// right column
-		if((board[3] == board[6]) && (board[6] == board[9]))
-			result = board[3];
-		System.out.println(board[3]+" wins");
-		// Check diagonals
-		// one diagonal
-		if((board[1] == board[5]) && (board[5] == board[9]))
-			result = board[1];
-		System.out.println(board[1]+" wins");
-		// the other diagonal
-		if((board[3] == board[5]) && (board[5] == board[7]))
-			result = board[3];
-		System.out.println(board[3]+" wins");
-			   
-	}
-
-	private static void play() {
-		while (true) {
-			if (board_filled == 9) {
-				System.out.println("It's a draw!");
-				System.exit(0);
-				break;
+	private static void winTie() {
+		displayBoard();
+		for (int index = 1; index < 10; index++) { // for loop
+			if (board[index] != ' ') { // if statement
+				if ((board[1] == board[2]) && (board[2] == board[3]))
+					result = board[1];
+	//		System.out.println(board[1]+" wins");
+				// middle row
+				if ((board[4] == board[5]) && (board[5] == board[6]))
+					result = board[4];
+	//		System.out.println(board[4]+" wins");
+				// bottom row
+				if ((board[7] == board[8]) && (board[8] == board[9]))
+					result = board[7];
+	//		System.out.println(board[7]+" wins");
+				/*
+				 * Check verticals left column
+				 */
+				if ((board[1] == board[3]) && (board[3] == board[7]))
+					result = board[1];
+	//	System.out.println(board[1]+" wins");
+				// middle column
+				if ((board[2] == board[5]) && (board[5] == board[8]))
+					result = board[2];
+	//	System.out.println(board[2]+" wins");
+				// right column
+				if ((board[3] == board[6]) && (board[6] == board[9]))
+					result = board[3];
+	//	System.out.println(board[3]+" wins");
+				/*
+				 * Check diagonals one diagonal
+				 */
+				if ((board[1] == board[5]) && (board[5] == board[9]))
+					result = board[1];
+	//	System.out.println(board[1]+" wins");
+				// the other diagonal
+				if ((board[3] == board[5]) && (board[5] == board[7]))
+					result = board[3];
+	//	System.out.println(board[3]+" wins");
 			}
-			toss();
 		}
-		while (board_filled < 9) {
-			if (turn == "user") {
+		
+		if(result == 'X') 
+			System.out.println(result+ " won the game!");
+		else if(result == 'X')
+			System.out.println(result+ " won the game!");
+		else {
+			if (turn == "user") 
 				compTurn();
-			} else
+			else
 				takePosition();
 		}
-		 winnerCheck();
 	}
 
+		public static void blockWinner(){
+			int position;
+			for(int index = 1; index < 10; index++) {
+				if(board[index] == index && board[index + 1] == index + 1) {
+					position = index + 2;
+				} else if (board[index + 2] ==index + 2 && board[index + 1] == index + 1) {
+					position = index;
+				} else if (board[index] == index && board[index + 2] == index + 3) {
+					position = index + 1;
+				}
+			}
+			compTurn();
+		}
 	// Calls method to take input for position and displaying board.
 	public static void main(String[] args) {
-
 		System.out.println("*****Welcome to the TicTacToe Game.*****");
 		inputLetter();
-		display();		
-		play();
+		display();
+		toss();
 	}
 }
